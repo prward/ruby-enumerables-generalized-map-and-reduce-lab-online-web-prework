774fbe0aef638c54394845b9765e81d518ea9e01
Patrick Ward <patrick.r.ward@verizon.net>
2019-09-24 14:25:33 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,7 +3,10 @@ def map(element1, &block)
   element1.map(&block)
   end
 
-def reduce(element1, starting_point = 0, &block)
-  element1.reduce(starting_point) { |sum, num| sum + num}
+def reduce(elements, starting_point = nil, &block)
+  if starting_point.nil?
+    elements.reduce(&block)
+  else
+    elements.reduce(starting_point, &block)
+  end
 end
-
