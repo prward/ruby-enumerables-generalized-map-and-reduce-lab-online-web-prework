d7c4854b681e7caf35096e3b625aae1f099c2014
Patrick Ward <patrick.r.ward@verizon.net>
2019-09-24 14:27:11 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,12 @@
 # Your Code Here
+def map(element1, &block)
+  element1.map(&block)
+  end
+
+def reduce(elements, starting_point = nil, &block)
+  if starting_point.nil?
+    elements.reduce(&block)
+  else
+    elements.reduce(starting_point, &block)
+  end
+end
