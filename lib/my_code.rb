99d64944d9e4d8ab2b5a5a47f32688d65c98e17d
Patrick Ward <patrick.r.ward@verizon.net>
2019-09-24 14:25:07 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,6 +3,6 @@ def map(element1, &block)
   element1.map(&block)
   end
 
-def reduce(element1, starting_point = nil, &block)
+def reduce(element1, starting_point = 0, &block)
   element1.reduce(starting_point) { |sum, num| sum + num}
 end
